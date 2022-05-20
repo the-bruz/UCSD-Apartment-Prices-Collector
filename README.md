1769196d2acaa13463556b8c1b5c307ee7d6e799
the-bruz <bruce.zrc@gmail.com>
2022-05-20 16:55:06 -0700

    Support Regents LaJolla

--- README.md
+++ README.md
@@ -5,12 +5,16 @@ This is a crawler program written in Python to collect the prices of popular apa
 Bruce Zhou
 
 ## Getting Started
-For windows user, directly use the .exe file in the dist directory.  
+For windows user, directly use the .exe file.  
   
 For manual compiling:
-* Compile user_interface.py.  
+* Compile user_interface.py in the Source folder.  
 * Make sure that "pyqt5" and "beautifulsoup4" packages are installed.  
 
 ## Update Log
-5/20/2022: Add an user interface. Add an executable file for windows.  
-5/19/2022: The program is now able to collect rent info from all garden community apartments.
+1.2 5/20/2022
+* Support Regents LaJolla now. Print the infos in a better-looking table.
+1.1 5/20/2022
+* Add an user interface. Add an executable file for windows.  
+1.0 5/19/2022
+* The program is now able to collect rent info from all garden community apartments.
---
+++ Source/user_interface.py
@@ -0,0 +1,64 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QComboBox, QLabel, QPushButton, QWidget\
+                            ,QHBoxLayout, QVBoxLayout
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QIcon
+import price_collector
+from tabulate import tabulate
+
+def search():
+    apt_list = ''
+    curr = comm_box.currentText()
+    if(curr ==  'Costa Verde Village'):  apt_list = price_collector.collect_cvv()
+    if(curr ==  'Towers at Costa Verde'):  apt_list = price_collector.collect_towers()
+    if(curr ==  '360 Luxury'):  apt_list = price_collector.collect_360_lux()
+    if(curr ==  'Lux UTC'):  apt_list = price_collector.collect_lux()
+    if(curr ==  'La Jolla Crossroads'):  apt_list = price_collector.collect_crossroads()
+    if(curr ==  'La Regencia'):  apt_list = price_collector.collect_la_regencia()
+    if(curr ==  'Regents LaJolla'):  apt_list = price_collector.collect_regents_lajolla()
+    infos = list_info(apt_list)
+    if (infos == 'No avaliable apartment found.'): result_label.setText(infos)
+    else: result_label.setText(tabulate(infos))
+
+def list_info(list):
+    if (len(list) == 0):
+        return 'No avaliable apartment found.'
+    info = []
+    info.append(list[0].table_header())
+    for a in list:
+        info.append(a.info_table())
+    return info
+
+
+app = QApplication([])
+app.setStyle('Fusion')
+
+comm_box = QComboBox()
+comm_box.addItems(['Costa Verde Village', 'Towers at Costa Verde', '360 Luxury', 
+'Lux UTC', 'La Jolla Crossroads', 'La Regencia', 'Regents LaJolla'])
+comm_label = QLabel("Community:")
+comm_label.setBuddy(comm_box)
+
+search_button = QPushButton('Search')
+result_label = QLabel()
+search_button.clicked.connect(search)
+
+top_widget = QWidget()
+top_layout = QHBoxLayout()
+top_layout.addWidget(comm_label)
+top_layout.addWidget(comm_box, Qt.AlignLeft)
+top_layout.addWidget(search_button)
+top_widget.setLayout(top_layout)
+
+
+window = QWidget()
+window_layout = QVBoxLayout()
+window_layout.setAlignment(Qt.AlignTop)
+window_layout.addWidget(top_widget)
+window_layout.addWidget(result_label)
+window.setLayout(window_layout)
+window.setFixedWidth(1000)
+window.setFixedHeight(600)
+window.show()
+app.setWindowIcon(QIcon('..\images\icon.ico'))
+app.exec()
